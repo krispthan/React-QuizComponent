b0aa46e0b556b85e82aa1d024e181f1c540855c2
Christine Than <44071872+krispthan@users.noreply.github.com>
2021-12-14 15:00:04 -0800

    render quiz question button

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,13 +13,17 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options.map((answer, index) => {
-              <QuizQuestionButton
-                clickHandler={this.handleClick.bind(this)}
-                key={index}
-                button_text={answer.answer_options}
-              />;
-            })}
+            {this.props.quiz_question.answer_options.map(
+              (answer_options, index) => {
+                return (
+                  <QuizQuestionButton
+                    clickHandler={this.handleClick.bind(this)}
+                    key={index}
+                    button_text={answer_options}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
